9125d55945119ae12119a41c58d8790a02cba48f
liutaotao <lttzzlll@gmail.com>
2014-09-25 21:27:53 +0800

    quick sort failed !

--- divide-and-conquer/quick_sort.c
+++ divide-and-conquer/quick_sort.c
@@ -5,6 +5,8 @@
 
 int input_data[100];
 
+void show(int *arr, int end, int start);
+
 void swap(int *a, int *b) {
 	int t = *a;
 	*a = *b;
@@ -81,16 +83,67 @@ int partition(int *arr, int left, int right) {
 }
 */
 
-// quick sort define 
-void quick_sort(int *arr, int left, int right) {
-	if ( left == right) {
-		return ;
+
+/*
+// return a perpor pivot
+// note that this version select the first item as pivot
+int partition(int *arr, int left, int right) {
+	int sential = arr[left];
+	//printf("povit is %d\n", sential);
+	int i = left;
+	int j = right;
+	while(i!=j) {
+		while(i!=j && arr[i]<=sential) {
+			i++;
+		}
+		while(i!=j && arr[j]>=sential) {
+			j--;
+		}
+		if(i!=j) {
+			swap(&arr[i], &arr[j]);
+		}
 	}
 
-	int pivot = partition(arr, left, right);
-	quick_sort(arr, left, pivot);
-	quick_sort(arr, pivot+1, right);
+	swap(&arr[i-1], &arr[left]);
 
+	return i-1;
+
+}
+*/
+
+// quick sort define 
+void quick_sort(int *arr, int left, int right) {
+	// element < 3 count 
+	int i, j;
+	if ( left + 2 >= right) {
+		for(i=left; i<=right; i++) {
+			for(j=i+1; j<=right; j++) {
+				if(arr[i]>arr[j]) {
+					swap(&arr[i], &arr[j]);
+				}
+			}
+		}
+		return ;
+	}
+	int pivot = arr[left];
+	i = left+1;
+	j = right;
+
+	while(i!=j) {
+		while(i!=j && arr[i] <= pivot) {
+			i++;
+		}
+		while(i!=j && arr[j] > pivot) {
+			j--;
+		}
+		if(i!=j) {
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[left], &arr[i-1]);
+
+	quick_sort(arr, left, i-1);
+	quick_sort(arr, i, right);
 }
 
 // display a array
@@ -106,10 +159,19 @@ int main() {
 	for(int i=0; i<100;i++) {
 		input_data[i] =rand() % 100;
 	}
+
+	printf("origin array\n");
 	show(input_data, 99, 0);
+
+	// test
+	//partition(input_data, 0, 99);
+	//printf("after partition\n");
+	//show(input_data, 99, 0);
+	//return 0;
 	
 	quick_sort(input_data, 0, 99);
 
+	printf("after sort\n");
 	show(input_data, 99, 0);
 
 	return 0;
