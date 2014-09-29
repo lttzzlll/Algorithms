f8606ede911b9156f2be94018b567f17d668837e
liutaotao <lttzzlll@gmail.com>
2014-09-29 17:18:39 +0800

    finish using quick sort to solve find-k-element

--- divide-and-conquer/seach-Kth-element-using-quick-sort/seach-Kth-element-using-quick-sort.c
+++ divide-and-conquer/seach-Kth-element-using-quick-sort/seach-Kth-element-using-quick-sort.c
@@ -2,8 +2,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<assert.h>
 
 int input_data[100];
+
+/* cnt global var define here for search count */
+int cnt = 0;
+
 
 void show(int *arr, int left, int right) {
 	printf("before display the array\n");
@@ -21,20 +26,20 @@ void swap(int *a, int *b) {
 
 void bubble_sort(int *arr, int left, int right) {
 	// bubble sort define here
-	for(int i=0; i<n; i++) {
-		for(int j=i+1; j<n; j++) {
-			if(arr[j] > arr[i] ) {
+	for(int i=left; i<=right; i++) {
+		for(int j=i+1; j<=right; j++) {
+			if(arr[i] > arr[j] ) {
 				swap(&arr[j], &arr[i]);
 			}
 		}
 	}
 }
 
-void random_unique(int *arr, int n, int up_limit) {
+void random_unique(int *arr, int n, int limit) {
 	srand((unsigned)time(NULL));
 	for(int i=0; i<n; i++) {
 		for(int k=0; k<i;) {
-			arr[i] = rand() % up_limit;
+			arr[i] = rand() % limit;
 			for(k=0; k<i; k++) {
 				if(arr[i] == arr[k]) {
 					break;
@@ -47,13 +52,65 @@ void random_unique(int *arr, int n, int up_limit) {
 	}
 }
 
-int search_k_element(int *arr, int left, int right, int k) {
-	if(left+k>right) {
-		bubble_sort(arr, left, right);
-		return arr[k-1];
+int find_perpor_pivot_return_index(int *arr, int left, int right) {
+	if(left+1>=right) {
+		return left;
+	}
+	int middle = (right-left)/2 + left;
+	if(arr[middle] >= arr[left] && arr[middle] <= arr[right]) {
+		return middle;
+	}else if(arr[left] >= arr[middle] && arr[left] <= arr[right]) {
+		return right;
+	}else {
+		return right;
+	}
+}
+
+void search_k_element(int *arr, int left, int right, int k) {
+	if(left==right) {
+		if(left==right) {
+			return ;
+		}else {
+			printf("error\n");
+			exit(-1);
+		}
 	}
 
+	int v, i, j;
+	
+	/* will be modify later */
+	v = arr[left];
+
+	i = left + 1;
+	j = right;
+
+	for(;;) {
+		while(arr[i]<v) {
+			i++;
+		}
+		while(arr[j]>v) {
+			j--;
+		}
+		if(i<j) {
+			swap(&arr[i], &arr[j]);
+			i++;
+			j--;
+		}else {
+			break;
+		}
+	}
+	swap(&arr[j], &arr[left]);
+	printf("in search %d times\n", cnt++);
+	if(k <= j-left) {
+		search_k_element(arr, left, j-1, k);
+	}else if(k==j-left+1) {
+		// problem occurs here
+		return ;
+	}else {
+		search_k_element(arr, j+1, right, k-(j-left)-1);
+	}
 }
+
 void random_range(int *arr, int n, int min_limit) {
 	srand((unsigned)time(NULL));
 	for(int i=0; i<n; i++) {
@@ -63,8 +120,8 @@ void random_range(int *arr, int n, int min_limit) {
 
 int main() {
 
-	int select, n, up_limit, min_limit, m, k;
-	select = 0;
+	int select, n, up_limit, min_limit, m;
+	select = 1;
 	n = 100;
 	up_limit = n*100;
 	min_limit = n; 
@@ -73,16 +130,26 @@ int main() {
 	}else{ 
 		random_range(input_data, n, min_limit);
 	}
-	
-	m = 50;
 
-	k = seach_k_element(input_data, 0, n-1, m);
-	
-	printf("%dth element is %d\n", m-1, k);
-
-	bubble_sort(input_data, 0, n-1);
-	printf("%dth element is %d\n", m-1, k);
 	show(input_data, 0, n-1);
+
+	printf("enter a number between %d to %d: ", 1, n);
+	scanf("%d", &m);
+	if( m < 0 ||  m > n) {
+		printf("enter error!\n");
+		return -1;
+	}
+
+	// k is the result
+	search_k_element(input_data, 0, n-1, m);
+	
+	printf("%dth element is %d\n", m, input_data[m-1]);
+
+	// for check
+	bubble_sort(input_data, 0, n-1);
+	show(input_data, 0, n-1);
+	printf("%dth element is %d\n", m, input_data[m-1]);
+
 
 	return 0;
 }
