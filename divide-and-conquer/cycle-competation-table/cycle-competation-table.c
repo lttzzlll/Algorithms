f043eeb4d1f44767dfa61a4b4eae3f58fc23ff18
liutaotao <lttzzlll@gmail.com>
2014-10-10 22:57:13 +0800

    will be finished tomorrow! just little bug ; before file is correct

--- divide-and-conquer/cycle-competation-table/cycle-competation-table.c
+++ divide-and-conquer/cycle-competation-table/cycle-competation-table.c
@@ -28,22 +28,44 @@ void cycle(int k, int a[][65]) {
 }
 
 // failed
+// now succeed : the reason is that the martrix is [symmetrical]
 void cycle2(int k, int a[][65]) {
 	int n = 1;
 
+	// n is the population of the objects
 	for(int i=1; i<=k; i++) { n *= 2; }
+	// view the table first column is the number of each object
 	for(int i=1; i<=n; i++) { a[i][1] = i; }
 
+	/* how to understand the code is depend on how you observe the martrix
+	 * the martrix is symmetrical about coordinate and each symmetrical part
+	 * is obvious and how to generate he martrix ? 
+	 * will be review tomorrow
+	 */
+	// m is the step size
 	int m = 1;
+	// s is the looptimes
 	for(int s=1; s<=k; s++) {
 		n /= 2;
+		// t is the current number of each object
 		for(int t=1; t<=n; t++) {
+
+			/* the copy part define here */
+			// i  and j  loop is used for copy to each symmetrical part
 			for(int i=m+1; i<=2*m; i++) {
 				for(int j=m+1; j<=2*m; j++) {
 					// down right     =   up    left
-					a[i][j+(t-1)*m*2] = a[i-m][j+(t-1)*m*2-m];
+//					a[i][j+(t-1)*m*2] = a[i-m][j+(t-1)*m*2-m];
 					// up   right     =   down  left
-					a[i-m][j+(t-1)*m*2] = a[i][j+(t-1)*m*2-m];
+//					a[i-m][j+(t-1)*m*2] = a[i][j+(t-1)*m*2-m];
+
+					// differ
+					// coordinate [symmetrical]!
+					// explicity compliant
+					//   down     right       up        left
+					a[i+(t-1)*m*2][j] = a[i+(t-1)*m*2-m][j-m];
+					//   up         right     down      left
+					a[i+(t-1)*m*2-m][j] = a[i+(t-1)*m*2][j-m];
 				}
 			}
 		}
@@ -64,7 +86,13 @@ void display(int arr[][65], int k) {
 
 int main() {
 	int k = 3;
-	cycle(k, a);
+	printf("enter k===> n=2**k: ");
+	scanf("%d", &k);
+	if(k>4 || k<0) {
+		printf("k(%d) is too big or too small\n", k);
+		exit(-1);
+	}
+	cycle2(k, a);
 	display(a, k);
 	return 0;
 }
---
+++ divide-and-conquer/cycle-competation-table/table-of-competation.c
@@ -0,0 +1,56 @@
+// cycle competation 
+#include<stdio.h>
+
+int a[129][129];
+
+void cycle(int a[][129], int k) {
+	int n = 1;
+	for(int i=1; i<=k; i++) { n *= 2; }
+
+	for(int i=1; i<=n; i++) { a[i][1] = i; }
+
+	// two loop: one is for population, other is for copy
+	for(int t=1; t<n; t*=2) { // 1 2 4 
+		// [right][down] = [left][up]
+		// copy scope
+		for(int i=0; i<t; i++) {
+			for(int j=0; j<t; j++) {
+				// [right][down] = [left][up]
+				a[i+t+1][j+t+1] = a[i+1][j+1];
+				printf("[%d, %d](%d) := [%d, %d](%d)  ", i+t+1, j+t+1, a[i+t+1][j+t+1], i+1, j+1, a[i+1][j+1]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for(int i=0; i<t; i++) {
+			for(int j=0; j<t; j++) {
+				// [right][up] := [left][down]
+				a[i+1][j+t+1] = a[i+t+1][j+1];
+				printf("[%d, %d](%d) := [%d, %d](%d)  ", i+1, j+t+1, a[i+1][j+t+1], i+t+1, j+1, a[i+t+1][j+1]);
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+}
+
+void display(int a[][129], int k) {
+	int n = 1;
+	for(int i=1; i<=k; i++) { n *= 2; }
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main() {
+	int k;
+	printf("enter k==> n = 2**k: ");
+	scanf("%d", &k);
+	cycle(a, k);
+	display(a, k);
+	return 0;
+}
