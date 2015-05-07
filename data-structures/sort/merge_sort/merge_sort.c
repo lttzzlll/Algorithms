52194a2a865fb0f312ce893a5e3adffb3cc9f521
liutaotao <lttzzlll@icloud.com>
2015-05-07 20:52:03 +0800

    add merge sort not finished

--- data-structures/sort/merge_sort/merge_sort.c
+++ data-structures/sort/merge_sort/merge_sort.c
@@ -1,3 +1,4 @@
+#include "merge_sort.h"
 #include <stdio.h>
 
 void merge_sort(int *arr, int n) {
---
+++ data-structures/sort/merge_sort/merge_sort.h
@@ -0,0 +1,6 @@
+#ifndef __MERGE_SORT_H__
+#define __MERGE_SORT_H__
+
+void sort(int *arr, int n);
+
+#endif
